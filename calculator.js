6cb9e8ed831958766fe38e5b95a541f80c3b2abd
Hridaan Tiwari <hridaantiwari3@gmail.com>
2025-06-21 14:56:09 +0545

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -7,75 +7,12 @@ btns.forEach(btn => {
     btn.addEventListener("click", () => {
         let tagname = btn.getAttribute("id");
 
-        if (tagname === "plus") {
-            display.innerText = "+";
-            cal = plus.innerText;
-        } 
-        else if (tagname === "sub") {
-            display.innerText = "-";
-            cal = sub.innerText;
-        }
-        else if (tagname === "mul") {
-            display.innerText = "×";
-            cal = mul.innerText;
-        }
-        else if (tagname === "div") {
-            display.innerText = "÷";
-            cal = div.innerText;
-        }
-        else if (tagname === "one") {
-            display.innerText = "1";
-            cal = one.innerText;
-        }
-        else if (tagname === "two") {
-            display.innerText = "2";
-            cal = two.innerText;
-        }
-        else if (tagname === "three") {
-            display.innerText = "3";
-            cal = three.innerText;
-        }
-        else if (tagname === "four") {
-            display.innerText = "4";
-            cal = four.innerText;
-        }
-        else if (tagname === "five") {
-            display.innerText = "5";
-            cal = five.innerText;
-        }
-        else if (tagname === "six") {
-            display.innerText = "6";
-            cal = six.innerText;
-        }
-        else if (tagname === "seven") {
-            display.innerText = "7";
-            cal = sevem.innerText;
-        }
-        else if (tagname === "eight") {
-            display.innerText = "8";
-            cal = eight.innerText;
-        }
-        else if (tagname === "nine") {
-            display.innerText = "9";
-            cal = nine.innerText;
-        }
-        else if (tagname === "rst") {
-            display.innerText = "";
-        }
-        else if (tagname === "di") {
-            display.innerText = "/";
-            cal = div.innerText;
-        }
-        else if (tagname === "zero") {
-            display.innerText = "0";
-        }
-
-        if (tagname !== "rst" && btn.innerText !== "=") {
+         if (tagname !== "rst" && btn.innerText !== "=") {
             expression += btn.innerText;
             display.innerText = expression;
         }
 
-        if (btn.innerText === "=") {
+ if (btn.innerText === "=") {
             try {
                 let safeExpr = expression.replace(/×/g, "*").replace(/÷/g, "/");
                 let result = eval(safeExpr);
@@ -87,7 +24,7 @@ btns.forEach(btn => {
             }
         }
 
-        if (tagname === "rst") {
+         if (tagname === "rst") {
             expression = "";
             display.innerText = "";
         }
